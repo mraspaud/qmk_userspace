5998a15762121453f9fff0d022721e55ecbb0436
Martin Raspaud <martin.raspaud@smhi.se>
2026-03-11 15:47:41 +0100

    Exchange pgup and pgdn to match standard placement

--- keyboards/cantor/keymaps/mraspaud/keymap.c
+++ keyboards/cantor/keymaps/mraspaud/keymap.c
@@ -455,7 +455,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                       └───┘   └───┘
       */
     [L_NAV] = LAYOUT_split_3x6_3(
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                        KC_BRID, KC_PGDN, KC_UP,   KC_PGUP, KC_BRIU, KC_VOLU,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                        KC_BRID, KC_PGUP, KC_UP,   KC_PGDN, KC_BRIU, KC_VOLU,
         _______, KC_LGUI, OS_LALT, OS_LSFT, OS_LCTL, _______,                                      KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_VOLD,
         KC_F7,   KC_F9,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                                       _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, KC_MUTE,
                                             _______, _______, _______,                    _______, _______, _______
